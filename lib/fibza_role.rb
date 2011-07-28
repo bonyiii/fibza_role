43812595d649a6d97473a707a96ecb64ccbf1614
Bonyii <boni@twine.hu>
2011-07-28 13:44:08 +0200

    start using controller and action name funcitons, right class methods changed to instance methods

--- lib/fibza_role.rb
+++ lib/fibza_role.rb
@@ -1,7 +1,9 @@
-require "fibza_role/controller_additions"
+require_dependency "fibza_role/controller_additions"
+#require_dependency "fibza_role/exceptions"
+#require_dependency "fibza_role/user_additions"
 
 module FibzaRole
-#    autoload :ControllerAdditions, "fibza_role/controller_additions"
+##    autoload :ControllerAdditions, "fibza_role/controller_additions"
     autoload :AccessDenied, "fibza_role/exceptions"
     autoload :UserAdditions, "fibza_role/user_additions"
 end
